f0777fde0aad3ee3ab771d41e07fa882b55aa903
Harehn <harehn.kaundun@mail.mcgill.ca>
2018-07-24 12:36:11 -0400

    Adjusted CentreBox.js and tested output on leftRight.html

--- CentreBox.js
+++ CentreBox.js
@@ -1,21 +1,71 @@
 class centreBox{
 	constructor(){
 	this.right=true;
+	this.rightBox=new Box((canvas.width/2)-transformX(90),0,transformX(180),transformY(60));
+	this.leftBox=new Box((canvas.width/2)-transformX(90),60,transformX(180),transformY(60));
 	this.draw();
 	}
 	draw(){
-		context.fillStyle="white";
-		context.fillRect((canvas.width/2)-90,0,180,120);
+		//context.fillStyle="white";
+		//context.fillRect((canvas.width/2)-transformX(90),0,transformX(180),transformY(120));
+		
 		context.fillStyle="black";
-		context.rect((canvas.width/2)-90,0,180,60);
-		context.rect((canvas.width/2)-90,60,180,60);
+		context.rect((canvas.width/2)-transformX(90),0,transformX(180),transformY(60));
+		context.rect((canvas.width/2)-transformX(90),transformY(60),transformX(180),transformY(60));
 		context.stroke();
-		context.font="30px Arial";
-		context.fillText("left",(canvas.width/2)-90,20);
+		this.write();
+	}
+	
+	write(){
+	context.font="30px Arial";
+	context.fillText("left",(canvas.width/2)-transformX(20),transformY(40));
+	context.font="30px Arial";
+	context.fillText("right",(canvas.width/2)-transformX(30),transformY(100));
+	}
+	
+	stress(){
+		if(right){
+			this.rightBox.highlighted=true;
+			this.leftBox.highlighted=false;
+			
+			this.rightBox.highlight();
+			this.leftBox.highlight();
+		}else{
+			this.rightBox.highlighted=false;
+			this.leftBox.highlighted=true;
+			
+			this.rightBox.highlight();
+			this.leftBox.highlight();
+		}
+		this.draw();
+	}
+	changeDirection(){
+		this.right=!this.right;
 	}
 }
 
-
+class Box{
+	constructor(x,y,width,height){
+		this.highlighted=false;
+		this.x=x;
+		this.y=y;
+		this.width=width;
+		this.height=height;
+		//this.highlight();
+		context.fillStyle ="white";
+		context.fillRect(this.x,this.y,this.width,this.height);
+	}
+	highlight(){
+		context.fillStyle = this.highlighted? "white":"yellow"
+		context.fillRect(this.x,this.y,this.width,this.height);
+	}
+}
+function transformX(num){
+		return num*canvas.width/1200;
+	}
+function transformY(num){
+		return num*canvas.height/600;
+	}
 
 /*
 class User {
